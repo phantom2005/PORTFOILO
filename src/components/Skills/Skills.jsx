bdea58de4d55f07a2584ba9ad760f8f7e11e47bf
vaibhav.bandhan.ai.2005@gmail.com <vaibhav.bandhan.ai.2005@gmail.com>
2025-06-02 10:09:44 +0530

    added skill logo

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import './Skills.css';
 
-import commonSkillImage from '../../assets2/avatar/avatar.jpg';
+
+
+
+import a from '../../assets2/skilllogo/c++.jpg';
+import b from '../../assets2/skilllogo/css.jpg';
+import c from '../../assets2/skilllogo/express.jpg';
+import d from '../../assets2/skilllogo/firebase.jpg';
+import e from '../../assets2/skilllogo/git.jpg';
+import f from '../../assets2/skilllogo/html.jpg';
+import g from '../../assets2/skilllogo/java.jpg';
+import h from '../../assets2/skilllogo/js.jpg';
+import i from '../../assets2/skilllogo/mongodb.jpg';
+import j from '../../assets2/skilllogo/mysql.jpg';
+import k from '../../assets2/skilllogo/nodejs.jpg';
+import l from '../../assets2/skilllogo/pandas.jpg';
+import m from '../../assets2/skilllogo/postgres.jpg';
+import n from '../../assets2/skilllogo/reactlogo.jpg';
+import o from '../../assets2/skilllogo/vercel.jpg';
+import p from '../../assets2/skilllogo/vscode.jpg';
+import q from '../../assets2/skilllogo/sql.jpg';
+import r from '../../assets2/skilllogo/python.jpg';
+import s from '../../assets2/skilllogo/anaconda.jpg';
+
+
 
 const allSkills = [
-  { name: 'HTML', image: commonSkillImage },
-  { name: 'CSS', image: commonSkillImage },
-  { name: 'JavaScript', image: commonSkillImage },
-  { name: 'React.js', image: commonSkillImage },
-  { name: 'Node.js', image: commonSkillImage },
-  { name: 'Firebase', image: commonSkillImage },
-  { name: 'MongoDB', image: commonSkillImage },
-  { name: 'Git', image: commonSkillImage },
-  { name: 'VS Code', image: commonSkillImage },
+  { name: '', image:a },
+  { name: 'CSS', image:b },
+  { name: 'JavaScript', image:c },
+  { name: 'React.js', image:d },
+  { name: 'Node.js', image:e },
+  { name: 'Firebase', image:f },
+  { name: 'MongoDB', image:g },
+  { name: 'Git', image:h},
+  { name: 'VS Code', image:i },
+   { name: 'VS Code', image:j },
+    { name: 'VS Code', image:k },
+     { name: 'VS Code', image:l },
+      { name: 'VS Code', image:m },
+       { name: 'VS Code', image:n },
+        { name: 'VS Code', image:o }, 
+        { name: 'VS Code', image:p },
+         { name: 'VS Code', image:q }, 
+         { name: 'VS Code', image:r },
+          { name: 'VS Code', image:s },
+          
 ];
 
 const Skills = () => {
